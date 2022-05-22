e1efb88b8ff50524f2ec4e6fd10618af2ed8d5be
Marie <marieborisova@gmail.com>
2022-05-22 10:52:13 +0300

    1

---
+++ app/src/main/java/com/example/wardrobe2022client/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wardrobe2022client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
